2f552bd781ee79f781845074488175012ce1cf8c
skrimbleshanks <timwbodine@gmail.com>
2020-02-02 16:34:16 -0500

    fix array bug

--- javascript/pin_login.js
+++ javascript/pin_login.js
@@ -2,13 +2,15 @@ $(document).ready(() => {
     let pinput = [];
     let litpins = {};
     $("a").click(function() {
-        pinput.push($(this).text())
-        litpins = $(".pin-dot").slice(0,pinput.length);
-        litpins.addClass("filled");
+
         if (litpins.length === 6) {
             litpins.removeClass("filled");
             litpins = {};
             pinput = [];
+            return;
         }
+        pinput.push($(this).text())
+        litpins = $(".pin-dot").slice(0,pinput.length);
+        litpins.addClass("filled");
     })
 })
