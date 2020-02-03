47fb3b23531dc3066a3a4b3e62e5d8043cdc4737
skrimbleshanks <timwbodine@gmail.com>
2020-02-03 10:58:23 -0500

    clean up js

--- javascript/pin_login.js
+++ javascript/pin_login.js
@@ -2,8 +2,6 @@ $(document).ready(() => {
     let pinput = [];
     let litpins = {};
     $(".pin-button").click(function() {
-        console.log(pinput.length);
-        console.log(litpins.length);
         if (litpins.length === 6) {
             $(".pin-dots-box").addClass("reject");
             setTimeout(function(){ $(".pin-dots-box").removeClass("reject"); }, 500);
@@ -14,15 +12,13 @@ $(document).ready(() => {
         }
         pinput.push($(this).text().replace(/\s+/g, ''))
         litpins = $(".pin-dot").slice(0,pinput.length);
-        console.log(litpins);
         litpins.addClass("filled");
     })
 
     $(".login-button").click(function() {
-        if (pinput.join("").replace(/\s+/g, '') === "123456") {
+        if (pinput.join("") === "123456") {
             alert("Correct!");
         } else {
-            console.log(pinput.join("").replace(/\s+/g, ''));
             $(".pin-dots-box").addClass("reject");
             setTimeout(function(){ $(".pin-dots-box").removeClass("reject"); }, 500);
         }
@@ -33,12 +29,9 @@ $(document).ready(() => {
     })
 
     $(".back-button").click(function() {
-        let removedPin = pinput.length - 1;
         litpins.removeClass("filled");
-        pinput = pinput.slice(0, removedPin);
+        pinput.pop();
         litpins = $(".pin-dot").slice(0,pinput.length); 
         litpins.addClass("filled");
-        console.log(pinput.length);
-        console.log(litpins.length);
     })
 })
